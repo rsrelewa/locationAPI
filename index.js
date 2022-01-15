1508c3fe2536a220c95324bdf6e9059af6b227d9
raghid <rsrelewa@outlook.com>
2022-01-14 23:19:45 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+const path = require('path')
+
+const { Navigator } = require("node-navigator");
+const navigator = new Navigator();
+
+const { v4: uuidv4 } = require('uuid');
+
+const NodeGeocoder = require('node-geocoder');
+const geoOptions = {
+    provider: 'openstreetmap',
+    httpAdapter: 'https', // Default
+    apiKey: ' ', // for Mapquest, OpenCage, Google Premier
+    formatter: 'json' // 'gpx', 'string', ...
+};
+const geocoder = NodeGeocoder(geoOptions);
+
+let database = {}
+
+const latlong = navigator.geolocation.getCurrentPosition((success, error) => {
+        geocoder.reverse({lat:success.latitude, lon:success.longitude}, function(err, result) {
+            app.get('/location/api/ip',(req,res)=>{
+                database[result[0].formattedAddress] = [result[0].latitude,result[0].longitude]
+                res.send(result[0])
+            })
+        });
+});
+
+app.get('/location',(req,res)=>{
+    res.sendFile(path.join(__dirname, '../geolocation/public', 'index.html'))
+    console.log(database)
+})
+
+app.put('/location/',(req,res)=>{
+    res.send(database)
+})
+
+app.listen(PORT,() => console.log(`Listening at http://localhost:${PORT}`))
